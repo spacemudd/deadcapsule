96206ddc3bf93652bab56f7bb57baced017d8cb5
Shafiq al-Shaar <sean.spilot@gmail.com>
2019-03-03 05:15:01 +0300

    Add frontpage

---
+++ resources/lang/en/words.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'faq' => 'FAQ',
+    'sign-up' => 'Sign Up',
+    'sign-in' => 'Sign in',
+    'redirected-to-sign-up' => 'You will be redirected to the sign up page',
+];
